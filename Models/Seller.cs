791378bd2816f247da7b11086f01758806d8cc92
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-08-08 20:22:45 -0300

    Adding Seller and SalesRecord Entities and running second migration

---
+++ Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSale(SalesRecord sale)
+        {
+            Sales.Add(sale);
+        }
+
+        public void RemoveSale(SalesRecord sale)
+        {
+            Sales.Remove(sale);
+        }
+
+        public double TotalSales(DateTime initialDate, DateTime finalDate)
+        {
+            return Sales
+                .Where(sale => sale.Date >= initialDate && sale.Date <= finalDate)
+                .Sum(sale => sale.Amount);
+        }
+    }
+}
