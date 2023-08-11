d91347025f13b1e82c6f6d459c8d4e9b06c1fec0
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-08-11 03:55:32 -0300

    Adding SalresRecord controller and service and views in order to implement sales search

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? initialDate, DateTime? finalDate)
+        {
+            ValidateDates(ref initialDate, ref finalDate);
+
+            var sales = await _salesRecordService.FindByDateAsync(initialDate, finalDate);
+
+            FormatViewDataDates(initialDate.Value, finalDate.Value);
+
+            return View(sales);
+        }
+
+        public async Task<IActionResult> GroupingSearch(DateTime? initialDate, DateTime? finalDate)
+        {
+            ValidateDates(ref initialDate, ref finalDate);
+
+            var sales = await _salesRecordService.FindByDateGroupingAsync(initialDate, finalDate);
+
+            FormatViewDataDates(initialDate.Value, finalDate.Value);
+
+            return View(sales);
+        }
+
+        private void ValidateDates(ref DateTime? initialDate, ref DateTime? finalDate)
+        {
+            if (!initialDate.HasValue)
+                initialDate = new DateTime(DateTime.Now.Year, 1, 1);
+
+            if (!finalDate.HasValue)
+                finalDate = DateTime.Now;
+        }
+
+        public void FormatViewDataDates(DateTime initialDate, DateTime finalDate)
+        {
+            ViewData["initialDate"] = initialDate.ToString("yyyy-MM-dd");
+            ViewData["finalDate"] = finalDate.ToString("yyyy-MM-dd");
+        }
+    }
+}
---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? initialDate, DateTime? finalDate)
+        {
+            return await FindRecordsByDateAsync(initialDate, finalDate);
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? initialDate, DateTime? finalDate)
+        {
+            var result = await FindRecordsByDateAsync(initialDate, finalDate);
+
+            return result.GroupBy(record => record.Seller.Department).ToList();
+        }
+
+        private async Task<List<SalesRecord>> FindRecordsByDateAsync(DateTime? initialDate, DateTime? finalDate)
+        {
+            var result = from record in _context.SalesRecord select record;
+
+            if (initialDate.HasValue)
+                result = result.Where(record => record.Date >= initialDate.Value);
+
+            if (finalDate.HasValue)
+                result = result.Where(record => record.Date <= finalDate.Value);
+
+            return await result
+                .Include(record => record.Seller)
+                .Include(record => record.Seller.Department)
+                .OrderByDescending(record => record.Date)
+                .ToListAsync();
+        }
+    }
+}
